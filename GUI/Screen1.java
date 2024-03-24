c054de83acad1731d94c8a7f3fb13c19cc91f4f2
amankhnn <148470369+amankhnn@users.noreply.github.com>
2024-03-24 13:18:14 -0300

    Update Screen1.java

--- GUI/Screen1.java
+++ GUI/Screen1.java
@@ -1,13 +1,9 @@
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -22,21 +18,45 @@ public class screen1 extends Application {
         createBookingButton.setOnAction(e -> {
            
             System.out.println("Create Booking button clicked");
+
+           
+            Stage userInfoStage = new Stage();
+            userInfoStage.setTitle("User Info");
+
+           
+            Label nameLabel = new Label("Name:");
+            TextField nameField = new TextField();
+            Label emailLabel = new Label("Email:");
+            TextField emailField = new TextField();
+            Label phoneLabel = new Label("Phone Number:");
+            TextField phoneField = new TextField();
+            Label idLabel = new Label("Unique ID:");
+            TextField idField = new TextField();
+
+            
+            VBox formLayout = new VBox(10);
+            formLayout.getChildren().addAll(nameLabel, nameField, emailLabel, emailField, phoneLabel, phoneField, idLabel, idField);
+            formLayout.setAlignment(Pos.CENTER);
+
+            
+            Scene formScene = new Scene(formLayout, 300, 400);
+            userInfoStage.setScene(formScene);
+            userInfoStage.show();
         });
 
         Button modifyBookingButton = new Button("Modify Booking");
         modifyBookingButton.setOnAction(e -> {
-           
+            
             System.out.println("Modify Booking button clicked");
         });
 
         VBox layout = new VBox(10);
         layout.getChildren().addAll(createBookingButton, modifyBookingButton);
-        layout.setAlignment(Pos.CENTER); 
+        layout.setAlignment(Pos.CENTER);  
 
         StackPane root = new StackPane();
         root.getChildren().add(layout);
-        root.setAlignment(Pos.CENTER); 
+        root.setAlignment(Pos.CENTER);  
 
         Scene scene = new Scene(root, 700, 400);
         primaryStage.setScene(scene);
@@ -47,4 +67,5 @@ public class screen1 extends Application {
         launch(args);
     }
 }
+
 
